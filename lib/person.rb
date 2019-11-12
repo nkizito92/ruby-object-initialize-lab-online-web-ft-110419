657408e0d35919cf7a20ab4c21edbbfdb818bffa
Kizito Njoku <nkizito57@gmail.com>
2019-11-12 17:36:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,15 +2,15 @@ class Person
   def initialize(name) 
     @name = name 
   end
+  
   def name=(name)
     @name 
   end 
+  
   def name
     @name
   end
   
 end 
 
-person = Person.new 
-person.name = "James"
-person.name 
+person = Person.new("James")
